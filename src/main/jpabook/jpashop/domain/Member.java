c566aefbb0d56ef774a08ecfbf6699d7f5cbf021
mi-hwa <43jmih@naver.com>
2023-10-30 08:40:31 +0900

    실전 예제 2 - 연관관계 매핑 시작

--- src/main/jpabook/jpashop/domain/Member.java
+++ src/main/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,12 @@
 package src.main.jpabook.jpashop.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Data;
 
 @Data
@@ -16,5 +19,8 @@ public class Member {
   private Long id;
   private String name;
   private String city;
+  private String street;
   private String zipcode;
+  @OneToMany(mappedBy = "id")
+  private List<Order> orders = new ArrayList<>();
 }
--- src/main/jpabook/jpashop/domain/Order.java
+++ src/main/jpabook/jpashop/domain/Order.java
@@ -1,12 +1,17 @@
 package src.main.jpabook.jpashop.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -19,9 +24,17 @@ public class Order {
   @GeneratedValue
   @Column(name = "ORDER_ID")
   private Long id;
-  @Column(name = "MEMBER_ID")
-  private Long memberId;
+  @ManyToOne
+  @JoinColumn(name = "MEMBER_ID")
+  private Member member;
+  @OneToMany(mappedBy = "order")
+  private List<OrderItem> orderItems = new ArrayList<>();
   private LocalDateTime orderDate;
   @Enumerated(EnumType.STRING)
   private OrderStatus status;
+
+  public void addOrderItem(OrderItem orderItem) {
+    orderItems.add(orderItem);
+    orderItem.setOrder(this);
+  }
 }
